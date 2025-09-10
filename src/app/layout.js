abafcdf133232c5d9bbcfed9a3ab52a6888416b2
articnine9 <articnine9@gmail.com>
2025-09-10 13:39:08 +0530

    add meta data

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 "use client";
-import { GoogleTagManager } from "@next/third-parties/google";
+
+import Script from "next/script";
 
 import "../assets/css/bootstrap.min.css";
 import "../assets/css/all.min.css";
@@ -14,11 +15,11 @@ import "../assets/css/react-adjustment.css";
 import "../assets/css/module-css/about.css";
 import "../assets/css/module-css/footer.css";
 import "../assets/css/module-css/header.css";
+
 import Loading from "../components/Section/Common/Loading/Loading";
 import Footer from "../components/Section/Common/Footer";
 import Header from "../components/Section/Common/Header";
 import React, { useEffect, useState } from "react";
-import { Metadata } from "../components/Section/Common/Metadata/Metadata";
 import { usePathname } from "next/navigation";
 import ChatPopup from "@/components/ChatPopup";
 
@@ -30,15 +31,37 @@ export default function RootLayout({ children }) {
     setIsLoading(true);
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 200); // Adjust the loading duration as needed
+    }, 200); // Adjust loading duration as needed
 
     return () => clearTimeout(timer);
   }, [pathname]);
 
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager script */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M6PZHR4B');
+          `}
+        </Script>
+      </head>
       <body>
-        <GoogleTagManager gtmId="GTM-M6PZHR4B" />
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M6PZHR4B"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager */}
+
         <Loading isLoading={isLoading} />
         {!isLoading && (
           <>
