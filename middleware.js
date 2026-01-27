0683bbed177c128c8265d5fb6d7cf9049cd5005a
articnine9 <articnine9@gmail.com>
2026-01-27 11:55:44 +0530

    27-01 v2

--- middleware.js
+++ middleware.js
@@ -1,12 +1,11 @@
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
-  const url = req.nextUrl.clone();
   const host = req.headers.get("host");
 
-  // Redirect to www + HTTPS in one step
-  if (host === "bestechparts.ae" || req.nextUrl.protocol === "http:") {
-    url.protocol = "https:";
+  // Redirect only non-www to www
+  if (host === "bestechparts.ae") {
+    const url = req.nextUrl.clone();
     url.hostname = "www.bestechparts.ae";
     return NextResponse.redirect(url, 301);
   }
@@ -15,5 +14,5 @@ export function middleware(req) {
 }
 
 export const config = {
-  matcher: "/:path*", // run on all routes
+  matcher: "/:path*",
 };
