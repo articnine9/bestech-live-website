b6425cdb9086ef0d1227651a4e14700014d0a4fb
vini <sahaya.vineet@althisolutions.com>
2026-04-21 15:45:52 +0530

    blog updated

--- .next/dev/static/chunks/src_app_layout_78cdd4a3.js
+++ .next/dev/static/chunks/src_app_layout_78cdd4a3.js
@@ -2,11 +2,10 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/src_40e70bb2._.js",
-  "static/chunks/node_modules_next_338f4661._.js",
   "static/chunks/node_modules_react-icons_fa_index_mjs_0459ff00._.js",
   "static/chunks/node_modules_react-icons_bs_index_mjs_8a44a60a._.js",
   "static/chunks/node_modules_react-icons_lib_844c6c50._.js",
-  "static/chunks/node_modules_4b4a67db._.js",
+  "static/chunks/node_modules_cbf14cee._.js",
   "static/chunks/src_2476b43b._.css"
 ],
     source: "dynamic"
